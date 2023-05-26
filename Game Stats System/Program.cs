c727f4bcd2da93ac0d00a8e727f508d23dbb5da9
Fudge <fugenkenneth@gmail.com>
2023-05-26 15:27:35 +0800

    Added list and method

--- Game Stats System/Program.cs
+++ Game Stats System/Program.cs
@@ -6,206 +6,134 @@ namespace CMPE103OOP
     {
         static void Main()
         {
-        //Character Base Stats
-        int AiVit = 82; 
-        int AiStr = 42; 
-        int AiEnd = 56;
-        int AiAg = 37;
-        int AiInt = 50;
-        int AiDex = 45;
-        int LiVit = 85; 
-        int LiStr = 45; 
-        int LiEnd = 50;
-        int LiAg = 35;
-        int LiInt = 54;
-        int LiDex = 50;
-        
-        //Equipped Item Stats
-        int bVit = 37; 
-        int bStr = 34;
-        int bEnd = 0;
-        int bAg = 17; 
-        int bInt = 0;
-        int bDex = 19;
-        
-        //Character Overall Stats
-        int AoVit = AiVit + bVit;
-        int AoStr = AiStr + bStr;
-        int AoEnd = AiEnd + bEnd;
-        int AoAg = AiAg + bAg;
-        int AoInt = AiInt + bInt;
-        int AoDex = AiDex + bDex;
-        int LoVit = LiVit + bVit;
-        int LoStr = LiStr + bStr;
-        int LoEnd = LiEnd + bEnd;
-        int LoAg = LiAg + bAg;
-        int LoInt = LiInt + bInt;
-        int LoDex = LiDex + bDex;
+            // Character Base Stats
+            int[] EiStats = { 85, 45, 50, 35, 54, 50 };
+            int[] NiStats = { 84, 47, 50, 40, 49, 45 };
 
-        Console.WriteLine("------TEYVAT WELCOMES YOU!------");
-        Console.WriteLine("Choose a character:");
-        Console.WriteLine("Enter 1 to select Aether.");
-        Console.WriteLine("Enter 2 to select Lumine.");
-        
-        int charChoice;
-        while (true) {
-            Console.WriteLine("Enter your selection: ");
-            if (int.TryParse(Console.ReadLine(), out charChoice))
-        {
-            if (charChoice == 1 || charChoice == 2)
-        {
-            break;
-        }}
-            Console.WriteLine("Unrecognized Input. Try Again!");
+            // Equipped Item Stats
+            int[] bStats = { 37, 34, 0, 17, 0, 19 };
+
+            // Character Overall Stats
+            int[] EoStats = new int[6];
+            int[] NoStats = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                EoStats[i] = EiStats[i] + bStats[i];
+                NoStats[i] = NiStats[i] + bStats[i];
+            }
+
+            int EoVit = EoStats[0];
+            int EoStr = EoStats[1];
+            int EoEnd = EoStats[2];
+            int EoAg = EoStats[3];
+            int EoInt = EoStats[4];
+            int EoDex = EoStats[5];
+
+            int NoVit = NoStats[0];
+            int NoStr = NoStats[1];
+            int NoEnd = NoStats[2];
+            int NoAg = NoStats[3];
+            int NoInt = NoStats[4];
+            int NoDex = NoStats[5];
+
+            Console.WriteLine("—————— TEYVAT WELCOMES YOU! ——————");
+            Console.WriteLine();
+            Console.WriteLine("Choose a character:");
+            Console.WriteLine("Enter 1 to select Eos.");
+            Console.WriteLine("Enter 2 to select Nyx.");
+            Console.WriteLine();
+
+            int charChoice;
+            while (true)
+            {
+                Console.Write("Enter your selection: ");
+                if (int.TryParse(Console.ReadLine(), out charChoice))
+                {
+                    if (charChoice == 1 || charChoice == 2)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Unrecognized Input. Try Again!");
+            }
+            Console.WriteLine();
+
+            switch (charChoice)
+            {
+                case 1:
+                    GameStats("Eos", EiStats, bStats, EoStats);
+                    break;
+                case 2:
+                    GameStats("Nyx", NiStats, bStats, NoStats);
+                    break;
+            }
         }
 
-        switch (charChoice)
+        static void GameStats(string charName, int[] iStats, int[] bStats, int[] oStats)
         {
-            case 1:
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("------GAME CHARACTER STATS------");
-            Console.WriteLine("Enter 1 to View Base Stats.");
-            Console.WriteLine("Enter 2 to Check Equipped Item.");
-            Console.WriteLine("Enter 3 to View Overall Stats.");
-            Console.WriteLine("Enter 4 to Exit Game Stats.");
-            
-            while (true) {
-            Console.WriteLine("Enter your selection: ");
-            int statInput = Convert.ToInt32(Console.ReadLine());
-                if (statInput == 1)
-            {
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------Aether's Base Stats------" );
-                Console.WriteLine("Vitality: " + AiVit);
-                Console.WriteLine("Strength: " + AiStr);
-                Console.WriteLine("Endurance: " + AiEnd);
-                Console.WriteLine("Agility: " + AiAg);
-                Console.WriteLine("Intelligence: " + AiInt);
-                Console.WriteLine("Dexterity: " + AiDex);
+            int statInput;
 
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------GAME CHARACTER STATS------");
+            do
+            {
+                Console.WriteLine("—————— GAME CHARACTER STATS ——————");
                 Console.WriteLine("Enter 1 to View Base Stats.");
                 Console.WriteLine("Enter 2 to Check Equipped Item.");
                 Console.WriteLine("Enter 3 to View Overall Stats.");
                 Console.WriteLine("Enter 4 to Exit Game Stats.");
+                Console.WriteLine();
 
-            } else if (statInput == 2)    
-            {
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------Ceremonial War-Plume------");
-                Console.WriteLine("Vitality: +" + bVit);
-                Console.WriteLine("Strength: +" + bStr);
-                Console.WriteLine("Agility: +" + bAg);
-                Console.WriteLine("Dexterity: +" + bDex);
+                Console.Write("Enter your selection: ");
+                if (int.TryParse(Console.ReadLine(), out statInput))
+                {
+                    Console.WriteLine();
 
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------GAME CHARACTER STATS------");
-                Console.WriteLine("Enter 1 to View Base Stats.");
-                Console.WriteLine("Enter 2 to Check Equipped Item.");
-                Console.WriteLine("Enter 3 to View Overall Stats.");
-                Console.WriteLine("Enter 4 to Exit Game Stats.");
-
-            } else if (statInput == 3)
-            {
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("-----Aether's Overall Stats-----");
-                Console.WriteLine("Vitality: " + AoVit);
-                Console.WriteLine("Strength: " + AoStr);
-                Console.WriteLine("Endurance: " + AoEnd);
-                Console.WriteLine("Agility: " + AoAg);
-                Console.WriteLine("Intelligence: " + AoInt);
-                Console.WriteLine("Dexterity: " + AoDex);
-
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------GAME CHARACTER STATS------");
-                Console.WriteLine("Enter 1 to View Base Stats.");
-                Console.WriteLine("Enter 2 to Check Equipped Item.");
-                Console.WriteLine("Enter 3 to View Overall Stats.");
-                Console.WriteLine("Enter 4 to Exit Game Stats.");
-
-            } else if (statInput == 4)
-            {
-                Console.WriteLine("-------Session Terminated-------");
-                break;
-            }else 
-            {
-                Console.WriteLine("Unrecognized Input, Try Again!");
-            }}
-            break;
-            
-            case 2:
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("------GAME CHARACTER STATS------");
-            Console.WriteLine("Enter 1 to View Base Stats.");
-            Console.WriteLine("Enter 2 to Check Equipped Item.");
-            Console.WriteLine("Enter 3 to View Overall Stats.");
-            Console.WriteLine("Enter 4 to Exit Game Stats.");
-            
-            while (true) {
-            Console.WriteLine("Enter your selection: ");
-            int statInput = Convert.ToInt32(Console.ReadLine());
-                if (statInput == 1)
-            {
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------Lumine's Base Stats------" );
-                Console.WriteLine("Vitality: " + LiVit);
-                Console.WriteLine("Strength: " + LiStr);
-                Console.WriteLine("Endurance: " + LiEnd);
-                Console.WriteLine("Agility: " + LiAg);
-                Console.WriteLine("Intelligence: " + LiInt);
-                Console.WriteLine("Dexterity: " + LiDex);
-
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------GAME CHARACTER STATS------");
-                Console.WriteLine("Enter 1 to View Base Stats.");
-                Console.WriteLine("Enter 2 to Check Equipped Item.");
-                Console.WriteLine("Enter 3 to View Overall Stats.");
-                Console.WriteLine("Enter 4 to Exit Game Stats.");
-
-            } else if (statInput == 2)    
-            {
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------Ceremonial War-Plume------");
-                Console.WriteLine("Vitality: +" + bVit);
-                Console.WriteLine("Strength: +" + bStr);
-                Console.WriteLine("Agility: +" + bAg);
-                Console.WriteLine("Dexterity: +" + bDex);
-
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------GAME CHARACTER STATS------");
-                Console.WriteLine("Enter 1 to View Base Stats.");
-                Console.WriteLine("Enter 2 to Check Equipped Item.");
-                Console.WriteLine("Enter 3 to View Overall Stats.");
-                Console.WriteLine("Enter 4 to Exit Game Stats.");
-
-            } else if (statInput == 3)
-            {
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("-----Lumine's Overall Stats-----");
-                Console.WriteLine("Vitality: " + LoVit);
-                Console.WriteLine("Strength: " + LoStr);
-                Console.WriteLine("Endurance: " + LoEnd);
-                Console.WriteLine("Agility: " + LoAg);
-                Console.WriteLine("Intelligence: " + LoInt);
-                Console.WriteLine("Dexterity: " + LoDex);
-
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("------GAME CHARACTER STATS------");
-                Console.WriteLine("Enter 1 to View Base Stats.");
-                Console.WriteLine("Enter 2 to Check Equipped Item.");
-                Console.WriteLine("Enter 3 to View Overall Stats.");
-                Console.WriteLine("Enter 4 to Exit Game Stats.");
-
-            } else if (statInput == 4)
-            {
-                Console.WriteLine("-------Session Terminated-------");
-                break;
-            }else 
-            {
-                Console.WriteLine("Unrecognized Input, Try Again!");
-            }}
-            break;
+                    switch (statInput)
+                    {
+                        case 1:
+                            Console.WriteLine($"—————— {charName}'s Base Stats ——————");
+                            Console.WriteLine("Vitality: " + iStats[0]);
+                            Console.WriteLine("Strength: " + iStats[1]);
+                            Console.WriteLine("Endurance: " + iStats[2]);
+                            Console.WriteLine("Agility: " + iStats[3]);
+                            Console.WriteLine("Intelligence: " + iStats[4]);
+                            Console.WriteLine("Dexterity: " + iStats[5]);
+                            Console.WriteLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("—————— Ceremonial War-Plume ——————");
+                            Console.WriteLine("Vitality: +" + bStats[0]);
+                            Console.WriteLine("Strength: +" + bStats[1]);
+                            Console.WriteLine("Agility: +" + bStats[3]);
+                            Console.WriteLine("Dexterity: +" + bStats[5]);
+                            Console.WriteLine();
+                            break;
+                        case 3:
+                            Console.WriteLine($"—————— {charName}'s Overall Stats ——————");
+                            Console.WriteLine("Vitality: " + oStats[0]);
+                            Console.WriteLine("Strength: " + oStats[1]);
+                            Console.WriteLine("Endurance: " + oStats[2]);
+                            Console.WriteLine("Agility: " + oStats[3]);
+                            Console.WriteLine("Intelligence: " + oStats[4]);
+                            Console.WriteLine("Dexterity: " + oStats[5]);
+                            Console.WriteLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("—————— Session Terminated ——————");
+                            break;
+                        default:
+                            Console.WriteLine("Unrecognized Input, Try Again!");
+                            Console.WriteLine();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized Input, Try Again!");
+                    Console.WriteLine();
+                }
+            } 
+            while (statInput != 4);
         }
-    }    
-}}
+    }
+}
